151ff532f91bf7d234d09efdc5ecfcb300cfaa9f
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2024-11-29 20:34:45 +0330

    update

--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/Web-developing-team/user_management_module/user_management_model"
 )
 
 func main() {
 
+	fmt.Println(user_management_model.User{})
 	// Initialize Fiber app
 	app := fiber.New()
 
 	log.Fatal(app.Listen(":3000"))
+
 
 }
 
