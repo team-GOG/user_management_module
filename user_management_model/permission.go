49a779dfa02b4a120f53c16331ab516e803d40f6
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2024-11-30 10:10:49 +0330

    update permission model

--- user_management_model/permission.go
+++ user_management_model/permission.go
@@ -1,8 +1,17 @@
 package user_management_model
 
+import "gorm.io/gorm"
 
 type Permission struct {
     ID          uint   `gorm:"primaryKey"`
     Name        string `gorm:"unique;not null"`
 }
 
+// CreatePermissions inserts an array of permissions into the database
+func CreatePermissions(db *gorm.DB, permissions []Permission) error {
+    // Use GORM's Create method for batch insertion
+    if err := db.Create(&permissions).Error; err != nil {
+        return err
+    }
+    return nil
+}
