f6c9ae43d852190f9b72f29ff68cc62437ff060f
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2024-12-01 11:00:57 +0330

    update handler.go

--- user_management_handlers/handler.go
+++ user_management_handlers/handler.go
@@ -1,15 +1,15 @@
 package user_management_handlers
 
 import (
-	"gorm.io/gorm"
+    "gorm.io/gorm"
 
-	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2"
 )
 
 var db *gorm.DB
 
-func setDatabase(database *gorm.DB) {
-	db = database
+func SetDatabase(database *gorm.DB) {
+    db = database
 }
 
 // respondWithError is a helper to send error responses consistently.
