3f3ebe54fe56b4688cacbc120dc6b425ae7631f1
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2024-12-01 10:45:28 +0330

    add setDatabase function in handler

--- user_management_handlers/handler.go
+++ user_management_handlers/handler.go
@@ -1,13 +1,17 @@
 package user_management_handlers
 
 import (
+	"gorm.io/gorm"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/Web-developing-team/user_management_module/user_management_model"
 )
 
-var db = user_management_model.GetDB()
+var db *gorm.DB
 
-
+func setDatabase(database *gorm.DB) {
+	db = database
+}
 
 // respondWithError is a helper to send error responses consistently.
 func respondWithError(c *fiber.Ctx, status int, message string) error {
